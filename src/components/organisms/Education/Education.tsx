9423ecc8926218076ba6f7f96356dda7a55b04fb
alekseirechitskiy <slowspirit@gmail.com>
2023-09-01 22:32:15 +0300

    added accordion to the Education section

--- src/components/organisms/Education/Education.tsx
+++ src/components/organisms/Education/Education.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Education.module.scss';
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import EducationForm from '../../molecules/EducationForm';
 
-const Educatione = () => {
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+const Education = () => {
+  const [count, setCount] = useState([1]);
+
+  const render = count.map((item, index) => {
+    return (
+      <Accordion disableGutters sx={{ mb: 2 }}>
+        <AccordionSummary
+          // expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Education {index + 1}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <EducationForm />
+        </AccordionDetails>
+      </Accordion>
+    );
+  });
+
   return (
     <Box className={classes.education}>
-      <EducationForm />
-      <Button variant="contained">Add another Education</Button>
+      {count.length === 1 ? <EducationForm /> : render}
+
+      <Button onClick={() => setCount((prev) => [...prev, 1])} variant="contained">
+        Add another Education
+      </Button>
     </Box>
   );
 };
 
-export default Educatione;
+export default Education;
