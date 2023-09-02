9a45e57296bc1b88aa2bbc2ffd48d4ab7ff7d6a8
Pavel Kuzminskiy <fitsusus@gmail.com>
2023-09-02 10:08:08 +0300

    footer fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Footer from "./components/organisms/Footer";
 import Header from "./components/organisms/Header";
-import MainPage from "./pages/MainPage";
+import LandingBuilder from "./pages/LandingBuilder";
 
 function App() {
   return (
     <>
       <Header />
-      <MainPage />
+      <LandingBuilder />
       <Footer />
     </>
   );
