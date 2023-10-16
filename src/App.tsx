439a24cc136aea4b47c3a471d0c8f18a4ac488a2
Vladimir <sazanakov@inbox.ru>
2023-10-16 23:09:05 +0700

    fix bags

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Header from './components/organisms/Header';
 import LandingBuilder from './pages/LandingBuilder';
-import SliderSVPage from './pages/SliderSVPage';
 
 function App() {
   return (
     <>
       <Header />
       <LandingBuilder />
-      <SliderSVPage />
     </>
   );
 }
