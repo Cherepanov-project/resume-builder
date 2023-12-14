c439b76ef0bb19eff065af9cae9392d7dfdaef4c
LukyanenkoDmitriy <lukyanenkodmitriyal@gmail.com>
2023-12-14 19:16:01 +0300

    import

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/organisms/Header';
 import CvTemplatesPage from './pages/CvTemplatePage';
 import LandingBuilder from './pages/LandingBuilder';
+// import CvTemplate from "./pages/CvTemplate";
 
 function App() {
   return (
