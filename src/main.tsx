2fbf57b9b6381b406ba87e7891ea564ec702fad1
ALEX\fenik <aleksandr.cherepanov@rive.es>
2024-02-15 23:08:51 +0200

    123

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
