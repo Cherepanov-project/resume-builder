755ca0fd4c36f01782d9fee16e1f7e2756a601e0
ALEX\fenik <aleksandr.cherepanov@rive.es>
2024-02-15 23:49:25 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 // import LandingBuilder from './pages/LandingBuilder';
 // import SliderSV from './components/molecules/SliderSV';
 import CvTemplate from './pages/CvTemplate';
-// import StarterPage from './pages/StarterPage';
+import StarterPage from './pages/StarterPage';
 import { Route, Routes } from 'react-router-dom';
 // import SliderSV from './components/molecules/SliderSV';
 // import CvTemplate from './pages/CvTemplate';
