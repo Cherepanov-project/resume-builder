ef22e3f84f965aeb28ea49b26a9fc280194b2526
LeviAck <astarkin.semyon@gmail.com>
2024-05-27 15:11:57 +0300

    refactoring ElementSpecificSettings.tsx

--- src/components/molecules/ElementSpecificSettings/ElementSpecificSettings.tsx
+++ src/components/molecules/ElementSpecificSettings/ElementSpecificSettings.tsx
@@ -9,29 +9,26 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  SelectChangeEvent,
-  Stack,
-  TextField,
+  // FormControl,
+  // InputLabel,
+  // MenuItem,
+  // OutlinedInput,
+  // Select,
+  // SelectChangeEvent,
+  // Stack,
+  // TextField,
 } from '@mui/material';
 import { ExpandMore } from '@mui/icons-material';
 import Item from '@/components/atoms/StyledPaperItem';
+import ElementSpecificSettingsForm from '@components/molecules/ElementSpecificSettingsForm'
 import { getLabel } from '@/utils/labelUtils';
-// import { nanoid } from 'nanoid';
-// import { strict } from 'assert';
 
 const ElementSpecificSettings = () => {
   const layoutDate = useTypedSelector((state) => state.sectionsManager.layoutDate);
   const id: string = useTypedSelector((state) => state.sectionsManager.curId);
-
   let [r, w] = String(id).split('');
   let row: number = Number(r);
   let col: number = Number(w);
-
   let layoutRow = layoutDate[row];
   if (!layoutRow) {
     layoutRow = layoutDate[1];
@@ -44,17 +41,14 @@ const ElementSpecificSettings = () => {
     w = '1';
     col = 1;
   }
-
   type AccordionData = Array<[string, string]>;
-
-  let type: string = layoutElement.name || '';
-  let title: string = layoutElement.props.title || '';
-  let description: string = layoutElement.props.description || '';
-  let text: string = layoutElement.props.text || '';
-  let url: string = layoutElement.props.url || '';
-  let imgUrl: string = layoutElement.props.imgUrl || '';
-  let buttonText: string = layoutElement.props.buttonText || '';
-
+  const type: string = layoutElement.name || '';
+  const title: string = layoutElement.props.title || '';
+  const description: string = layoutElement.props.description || '';
+  const text: string = layoutElement.props.text || '';
+  const url: string = layoutElement.props.url || '';
+  const imgUrl: string = layoutElement.props.imgUrl || '';
+  const buttonText: string = layoutElement.props.buttonText || '';
 
   const dispatch = useDispatch();
 
@@ -130,162 +124,20 @@ const ElementSpecificSettings = () => {
       <Accordion>
         <AccordionSummary expandIcon={<ExpandMore />}>Configuration Settings</AccordionSummary>
         <AccordionDetails>
-          <form>
-            <Stack>
-              <Item>
-                <FormControl>
-                  <InputLabel id="type-label">Choose element type</InputLabel>
-                  <Select
-                    labelId="type-label"
-                    sx={{ width: '220px' }}
-                    value={type}
-                    onChange={(e: SelectChangeEvent<string>) => {
-                      type = e.target.value;
-                      handleUpdate('type', e.target.value, col - 1);
-                    }}
-                    input={<OutlinedInput label="Choose element type" />}
-                  >
-                    {settingsOptionsValues.map((name) => (
-                      <MenuItem key={name} value={name}>
-                        {name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Item>
-              {type !== 'LayoutBlockImage' && type !== 'LayoutBlockVideoPlayer' && type !== 'LayoutBlockSlider' &&  type !== 'Accordion'  && <Item>
-                <FormControl>
-                  <TextField
-                    label="Enter target text:"
-                    value={text}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                      text = e.target.value;
-                      handleUpdate('text', e.target.value, col - 1);
-                    }}
-                  />
-                </FormControl>
-              </Item>}
-              {type === 'Accordion' ? (
-                <Item>
-                  <FormControl>
-                    {accordion.map((item: string[], index: number) => (
-                      <div key={index}>
-                        <TextField
-                          label={`Enter target accordion ${index + 1} title:`}
-                          value={item[0]}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            const updatedAccordion = [...accordion];
-                            updatedAccordion[index] = [e.target.value, updatedAccordion[index][1]];
-                            setAccordion(updatedAccordion);
-                            handleUpdate('accordion', updatedAccordion, col - 1);
-                          }}
-                        />
-                        <TextField
-                          label={`Enter target accordion ${index + 1} description:`}
-                          value={item[1]}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            const updatedAccordion = [...accordion];
-                            updatedAccordion[index] = [updatedAccordion[index][0], e.target.value];
-                            setAccordion(updatedAccordion);
-                            handleUpdate('accordion', updatedAccordion, col - 1);
-
-                          }}
-                        />
-                      </div>
-                    ))}
-                    <button
-                      onClick={(e: { preventDefault: () => void; }) => {
-                        e.preventDefault();
-                        const updatedAccordion: AccordionData = [...accordion, ['', '']];
-                        setAccordion(updatedAccordion);
-                        handleUpdate('accordion', updatedAccordion, col - 1);
-                      }}
-                    >
-                      Add Item
-                    </button>
-                  </FormControl>
-                </Item>
-              ) : null}
-              {type === 'CardItem' ? (
-                <Item>
-                  <FormControl>
-                    <TextField
-                      label="Enter target title:"
-                      value={title}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        title = e.target.value;
-                        handleUpdate('title', e.target.value, col - 1);
-                      }}
-                    />
-                  </FormControl>
-                </Item>
-              ) : null}
-              {type === 'CardItem' ? (
-                <Item>
-                  <FormControl>
-                    <TextField
-                      label="Enter target description:"
-                      value={description}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        description = e.target.value;
-                        handleUpdate('description', e.target.value, col - 1);
-                      }}
-                    />
-                  </FormControl>
-                </Item>
-              ) : null}
-              {type === 'Anchor' ||
-              type === 'Avatars' ||
-              type === 'Image' ||
-              type === 'Gallery'  ||
-              type === 'VideoPlayer' ||
-              type === 'Slider' ||
-              type === 'SocialMediaIcon' ||
-              type === 'CardItem' ||
-              type === 'Logo'? (
-                <Item>
-                  <FormControl>
-                    <TextField
-                      label="Enter target URL:"
-                      value={url}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        url = e.target.value;
-                        handleUpdate('url', e.target.value, col - 1);
-                      }}
-                    />
-                  </FormControl>
-                </Item>
-              ) : null}
-              {type !== 'Image' && type !== 'VideoPlayer' && type !== 'Slider' && type === 'Logo' || type === 'SocialMediaIcon' || type === 'CardItem' ? (
-                <Item>
-                  <FormControl>
-                    <TextField
-                      label="Enter target image URL:"
-                      value={imgUrl}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        imgUrl = e.target.value;
-                        handleUpdate('imgUrl', e.target.value, col - 1);
-                      }}
-                    />
-                  </FormControl>
-                </Item>
-              ) : null}
-              {type === 'CardItem' ? (
-                <Item>
-                  <FormControl>
-                    <TextField
-                      label="Enter target button text:"
-                      value={buttonText}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        buttonText = e.target.value;
-                        handleUpdate('buttonText', e.target.value, col - 1);
-                      }}
-                    />
-                  </FormControl>
-                </Item>
-              ) : null}
-            </Stack>
-          </form>
+          <ElementSpecificSettingsForm
+            type={type}
+            text={text}
+            title={title}
+            description={description}
+            url={url}
+            imgUrl={imgUrl}
+            buttonText={buttonText}
+            accordion={accordion}
+            settingsOptionsValues={settingsOptionsValues}
+            handleUpdate={handleUpdate}
+            setAccordion={setAccordion}
+            col={col}
+          />
         </AccordionDetails>
       </Accordion>
       <Accordion>
---
+++ src/components/molecules/ElementSpecificSettingsForm/ElementSpecificSettingsForm.tsx
@@ -0,0 +1,177 @@
+import {
+  Stack,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  OutlinedInput,
+  TextField,
+  SelectChangeEvent,
+} from '@mui/material'
+import Item from '@/components/atoms/StyledPaperItem';
+
+type AccordionData = Array<[string, string]>
+
+const ElementSpecificSettingsForm = ({ type, text, title, description, url, imgUrl, buttonText, accordion, settingsOptionsValues, setAccordion, handleUpdate, col }) => {
+  return (
+    <form>
+      <Stack>
+        <Item>
+          <FormControl>
+            <InputLabel id="type-label">Choose element type</InputLabel>
+            <Select
+              labelId="type-label"
+              sx={{ width: '220px' }}
+              value={type}
+              onChange={(e: SelectChangeEvent<string>) => {
+                type = e.target.value;
+                handleUpdate('type', e.target.value, col - 1);
+              }}
+              input={<OutlinedInput label="Choose element type" />}
+            >
+              {settingsOptionsValues.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Item>
+        {type !== 'LayoutBlockImage' && type !== 'LayoutBlockVideoPlayer' && type !== 'LayoutBlockSlider' && type !== 'Accordion' &&
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target text:"
+                value={text}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  text = e.target.value;
+                  handleUpdate('text', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>}
+        {type === 'Accordion' ? (
+          <Item>
+            <FormControl>
+              {accordion.map((item: string[], index: number) => (
+                <div key={index}>
+                  <TextField
+                    label={`Enter target accordion ${index + 1} title:`}
+                    value={item[0]}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      const updatedAccordion = [...accordion];
+                      updatedAccordion[index] = [e.target.value, updatedAccordion[index][1]];
+                      setAccordion(updatedAccordion);
+                      handleUpdate('accordion', updatedAccordion, col - 1);
+                    }}
+                  />
+                  <TextField
+                    label={`Enter target accordion ${index + 1} description:`}
+                    value={item[1]}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      const updatedAccordion = [...accordion];
+                      updatedAccordion[index] = [updatedAccordion[index][0], e.target.value];
+                      setAccordion(updatedAccordion);
+                      handleUpdate('accordion', updatedAccordion, col - 1);
+
+                    }}
+                  />
+                </div>
+              ))}
+              <button
+                onClick={(e: { preventDefault: () => void; }) => {
+                  e.preventDefault();
+                  const updatedAccordion: AccordionData = [...accordion, ['', '']];
+                  setAccordion(updatedAccordion);
+                  handleUpdate('accordion', updatedAccordion, col - 1);
+                }}
+              >
+                Add Item
+              </button>
+            </FormControl>
+          </Item>
+        ) : null}
+        {type === 'CardItem' ? (
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target title:"
+                value={title}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  title = e.target.value;
+                  handleUpdate('title', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>
+        ) : null}
+        {type === 'CardItem' ? (
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target description:"
+                value={description}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  description = e.target.value;
+                  handleUpdate('description', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>
+        ) : null}
+        {type === 'Anchor' ||
+        type === 'Avatars' ||
+        type === 'Image' ||
+        type === 'Gallery' ||
+        type === 'VideoPlayer' ||
+        type === 'Slider' ||
+        type === 'SocialMediaIcon' ||
+        type === 'CardItem' ||
+        type === 'Logo' ? (
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target URL:"
+                value={url}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  url = e.target.value;
+                  handleUpdate('url', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>
+        ) : null}
+        {type !== 'Image' && type !== 'VideoPlayer' && type !== 'Slider' && type === 'Logo' || type === 'SocialMediaIcon' || type === 'CardItem' ? (
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target image URL:"
+                value={imgUrl}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  imgUrl = e.target.value;
+                  handleUpdate('imgUrl', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>
+        ) : null}
+        {type === 'CardItem' ? (
+          <Item>
+            <FormControl>
+              <TextField
+                label="Enter target button text:"
+                value={buttonText}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  buttonText = e.target.value;
+                  handleUpdate('buttonText', e.target.value, col - 1);
+                }}
+              />
+            </FormControl>
+          </Item>
+        ) : null}
+      </Stack>
+    </form>
+  );
+};
+
+export default ElementSpecificSettingsForm;
