e44ef3bca98a57347b54c4d253614e3295312d21
Parviz <Parvi3@yandex.ru>
2024-06-27 23:48:42 +0300

    refactor-CvTemplate

---
+++ src/pages/CvTemplate/components/ChoosingResumeOption.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { Box, Typography } from '@mui/material';
+
+import { TemplateSingleResume } from './index.ts';
+import { StylesNameKeys } from '@pages/CvTemplatePDF/const';
+
+interface IProps {
+  handleButtonClick: (nameResume: StylesNameKeys) => void;
+}
+
+const arrResume: StylesNameKeys[] = ['oslo', 'sydney', 'toronto'];
+
+const ChoosingResumeOption: FC<IProps> = ({ handleButtonClick }) => {
+  return (
+    <>
+      <Box>
+        <Typography
+          sx={{
+            mt: 4,
+            ml: 10,
+            mb: 2,
+            fontSize: 30,
+            fontWeight: 700,
+          }}
+        >
+          step 1 choose right way of info position
+        </Typography>
+        <Box display="flex" sx={{ ml: 10, mb: 10 }}>
+          {arrResume.map((r) => (
+            <TemplateSingleResume handleButtonClick={handleButtonClick} nameResume={r} />
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default ChoosingResumeOption;
