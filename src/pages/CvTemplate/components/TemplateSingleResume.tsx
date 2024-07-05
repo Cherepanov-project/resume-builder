a2a66863b238beec35b6b33c2a57f2bb39fa7e60
Parviz <Parvi3@yandex.ru>
2024-07-05 14:24:12 +0300

    feature/CvTemplate-parviz

--- src/pages/CvTemplate/components/TemplateSingleResume.tsx
+++ src/pages/CvTemplate/components/TemplateSingleResume.tsx
@@ -2,15 +2,23 @@ import { FC } from 'react';
 import { Box, Button } from '@mui/material';
 import { buttonStyle } from '@assets/style/buttonStyle.ts';
 
-import oslo from '../resumeTemplates/oslo-resume-templates.avif';
-import toronto from '../resumeTemplates/toronto-resume-templates.avif';
-import sydney from '../resumeTemplates/sydney-resume-templates.avif';
+import {
+  oslo,
+  classicCustomized,
+  modern,
+  sydney,
+  toronto,
+  defaultCustomized,
+} from '../resumeTemplates/index';
 import { StylesNameKeys } from '@pages/CvTemplatePDF/const';
 
 const objResume = {
   oslo,
   toronto,
   sydney,
+  modern,
+  classicCustomized,
+  defaultCustomized,
 };
 
 interface IProps {
@@ -20,9 +28,17 @@ interface IProps {
 
 const TemplateSingleResume: FC<IProps> = ({ nameResume, handleButtonClick }) => {
   return (
-    <Box display="flex" flexDirection="column" sx={{ mr: 5 }}>
-      <img src={objResume[nameResume]} alt={nameResume} style={{ width: '500px' }} />
-      <Button sx={buttonStyle} onClick={() => handleButtonClick(nameResume)}>
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <img
+        src={objResume[nameResume]}
+        alt={nameResume}
+        style={{ width: '500px', height: '707px' }}
+      />
+      <Button
+        style={{ width: '500px' }}
+        sx={buttonStyle}
+        onClick={() => handleButtonClick(nameResume)}
+      >
         Choose
       </Button>
     </Box>
