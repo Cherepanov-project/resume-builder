5cd05d456d7a15b7584322112d26bbca8d8fbd25
Maks Solovev <msol86@mail.ru>
2024-08-13 23:19:45 +0500

    Add LeftSide to LetterBuilderPage

--- src/pages/LetterBuilderPage/LetterBuilderPage.tsx
+++ src/pages/LetterBuilderPage/LetterBuilderPage.tsx
@@ -1,11 +1,17 @@
+import LetterBuilderLeftSide from '@/components/organisms/LetterBuilderLeftSide';
 import LetterConstructorWorkspace from '../../components/organisms/LetterConstructorWorkspace/LetterConstructorWorkspace';
 import LetterBuilderHeader from '@/components/molecules/LetterBuilderHeader';
+
+import classes from './LetterBuilderPage.module.scss'
 
 const LetterBuilderPage = () => {
   return (
     <>
       <LetterBuilderHeader />
-      <LetterConstructorWorkspace />
+      <section className={classes['main']}>
+        <LetterBuilderLeftSide />
+        <LetterConstructorWorkspace />
+      </section>
     </>
   )
 }
