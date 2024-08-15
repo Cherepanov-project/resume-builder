1d464f92767b8dc4af53aac345369daf5a573846
Maks Solovev <msol86@mail.ru>
2024-08-15 23:59:03 +0500

    1. Refactoring FullWidthTabs 2. Added 6 LetterCard

--- src/components/atoms/LetterCard/LetterCard.tsx
+++ src/components/atoms/LetterCard/LetterCard.tsx
@@ -1,21 +1,63 @@
-import classes from './LetterCard.module.scss'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 interface LetterCardProps {
     icon: JSX.Element
     text: string
 }
+
 const LetterCard = ({ icon, text }: LetterCardProps) => {
     return (
-        <div className={classes['letter-card']}>
-            <div className={classes['letter-card__content']}>
-                <div className={classes['letter-card__icon']}>
-                    {icon}
-                </div>
-                <span className={classes['letter-card__text']}>
-                    {text}
-                </span>
-            </div>
-        </div>
+      <Box 
+        sx={{
+          boxSizing: 'border-box',
+          width: 'calc(33.3% - 15px)',
+          background: '#fff',
+          borderRadius: '3px',
+          display: 'block',
+          minHeight: '118px',
+          padding: '18px 15px 15px 15px',
+          border: '1px solid #ccc',
+          boxShadow: '0 1px 1px rgba(0,0,0,0.1)',
+          transitionDuration: '.15s',
+          '&:hover': {
+            boxShadow: '0 6px 10px rgba(0, 0, 0, .35)',
+            border: '1px solid #ffffff',
+          }
+        }}>
+        <Box sx={{
+          boxSizing: 'border-box',
+          verticalAlign: 'top',
+          alignItems: 'center',
+          display: 'flex',
+          position: 'relative',
+          flexFlow: 'column',
+          justifyContent: 'center',
+        }}>
+          <Box sx={{
+            margin: '8px auto 15px auto',
+            flex: '1.9 0 66%',
+          }}>
+            {icon}
+          </Box>
+          <Box>
+            <Typography sx={{
+              hyphens: 'auto',
+              wordBreak: 'break-word',
+              wordWrap: 'break-word',
+              fontSize: '12px',
+              height: '26.4px',
+              lineHeight: '1.1',
+              marginTop: '4px',
+              overflow: 'hidden',
+              textAlign: 'center',
+              textTransform: 'uppercase',
+            }}>
+              {text}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     )
 }
 
--- src/components/organisms/LetterBuilderLeftSide/LetterBuilderLeftSide.tsx
+++ src/components/organisms/LetterBuilderLeftSide/LetterBuilderLeftSide.tsx
@@ -1,190 +1,75 @@
-import * as React from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import { useTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import LetterCard from '@/components/atoms/LetterCard';
+import FullWidthTabs from '@/components/molecules/FullWidthTabs';
+import { TabIconStrings, TabIconSettings, TabIconContent } from '@/components/atoms/Icons/TabIcons';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: number;
-  value: number;
-}
+const TabList = [
+  {
+    id: 0,
+    label: 'Содержимое',
+    icon: TabIconContent,
+  },
+  {
+    id: 1,
+    label: 'Строки',
+    icon: TabIconStrings,
+  },
+  {
+    id: 2,
+    label: 'Настройки',
+    icon: TabIconSettings,
+  }
+]
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+const LetterCardList = [
+  {
+    text: 'ЗАГОЛОВОК',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 30 26"><path fill="#515659" d="m10.849 4.217-.051-.223a3.547 3.547 0 0 0-.288-.746c-.15-.307-.3-.58-.466-.851a5.165 5.165 0 0 0-.563-.707c-.197-.185-.35-.266-.477-.29a23.179 23.179 0 0 0-.703-.051 12.724 12.724 0 0 0-.793-.017H7.26v10.445c0 .184.03.313.09.405l.01.015.008.016c.047.096.14.182.321.275.019.01.075.03.194.058.112.027.257.057.438.092.375.073.618.118.773.118h.286V14H2.603v-1.216l.257-.027.287-.025c.146-.012.32-.027.507-.045.347-.034.558-.082.663-.121.165-.072.25-.145.315-.243.058-.089.091-.214.091-.458V1.315h-.249c-.19 0-.422 0-.688.016-.275.018-.55.035-.806.053-.102.01-.245.086-.46.289a5.287 5.287 0 0 0-1.024 1.563c-.138.316-.233.56-.278.745l-.053.219H0V0h12v4.217h-1.152ZM16 0h14v2H16zM16 6h14v2H16zM16 12h14v2H16zM0 18h30v2H0zM0 24h30v2H0z"></path></svg>,
+    id: 0,
+  },
+  {
+    text: 'Параграф',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewBox="0 0 35 31"><path fill="#515659" d="M34.4 21.333H.6a.6.6 0 0 0-.6.6v1.134a.6.6 0 0 0 .6.6h33.8a.6.6 0 0 0 .6-.6v-1.134a.6.6 0 0 0-.6-.6ZM34.4 28.333H.6a.6.6 0 0 0-.6.6v1.134a.6.6 0 0 0 .6.6h33.8a.6.6 0 0 0 .6-.6v-1.134a.6.6 0 0 0-.6-.6ZM15.733.333H.6a.6.6 0 0 0-.6.6v1.134a.6.6 0 0 0 .6.6h15.133a.6.6 0 0 0 .6-.6V.933a.6.6 0 0 0-.6-.6ZM15.733 7.333H.6a.6.6 0 0 0-.6.6v1.134a.6.6 0 0 0 .6.6h15.133a.6.6 0 0 0 .6-.6V7.933a.6.6 0 0 0-.6-.6ZM19.733 14.333H.6a.6.6 0 0 0-.6.6v1.134a.6.6 0 0 0 .6.6h19.133a.6.6 0 0 0 .6-.6v-1.134a.6.6 0 0 0-.6-.6Z"></path><path fill="#000" fill-rule="evenodd" d="M33.56 2.858V16.13a.488.488 0 0 1-.476.498H31.66a.489.489 0 0 1-.475-.498V2.858H28.56v13.271a.488.488 0 0 1-.475.498h-1.422a.489.489 0 0 1-.476-.498v-4.871l-.05-1C23.3 10.258 21 8.31 21 5.288c0-3.502 2.654-4.96 5.678-4.96h7.847c.26 0 .475.228.475.507v1.518c0 .278-.214.506-.475.506h-.966v-.001Z" clip-rule="evenodd"></path></svg>,
+    id: 1,
+  },
+  {
+    text: 'Список',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" aria-hidden="true" data-icon="list" data-prefix="far" viewBox="0 0 512 512"><path fill="#515659" d="M80 48H16A16 16 0 0 0 0 64v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16V64a16 16 0 0 0-16-16zm0 160H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm0 160H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm416-136H176a16 16 0 0 0-16 16v16a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-16a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v16a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-16a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v16a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V88a16 16 0 0 0-16-16z"></path></svg>,
+    id: 2,
+  },
+  {
+    text: 'Картинки',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 28 28" width="36" height="36"><path fill="#515659" d="M0 0v28h28V0Zm26 2v7.24l-10.86 8.67-5.79-3L2 20.34V2ZM2 26v-4.42l7.45-5.5 5.81 3L26 10.52V26Zm7.8-13.9A3.8 3.8 0 1 0 6 8.3a3.8 3.8 0 0 0 3.8 3.8Zm0-6.6a2.8 2.8 0 1 1 0 5.6A2.73 2.73 0 0 1 7 8.3a2.93 2.93 0 0 1 2.8-2.8Z"></path></svg>,
+    id: 3,
+  },
+  {
+    text: 'Кнопка',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 51 30" width="36" height="36"><path fill="#515659" d="m46.6 29.2 1.3-.7-2.1-4h3.6L40 15.1v13.3l2.7-2.7 2.3 4.4 1.6-.9ZM39 12.69l1.71 1.71 6.51 6.51h1A2.83 2.83 0 0 0 51 18.07V2.83A2.83 2.83 0 0 0 48.17 0H2.83A2.83 2.83 0 0 0 0 2.83v15.24a2.83 2.83 0 0 0 2.83 2.83H39v-8.21ZM8.33 9.22h34.34v2.45H8.33Z"></path></svg>,
+    id: 4,
+  },
+  {
+    text: 'Table',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewBox="0 0 35 31"><path fill="#515659" d="M2.188 10.938v7.656h14.218v-7.657H2.188Zm16.406 0v7.656h14.218v-7.657H18.595Zm-2.188 9.843H2.188v7.657h14.218V20.78Zm2.188 7.657h14.218V20.78H18.595v7.657ZM0 0h35v30.625H0V0Z"></path></svg>,
+    id: 5,
+  }
+]
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ padding: '15px', background: '#f9f9f9', minHeight: '100%' }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
-
-interface FullWidthTabsProps {
-  ViewChild: JSX.Element
-}
-
-function FullWidthTabs({ ViewChild }: FullWidthTabsProps) {
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
-  };
-
-  return (
-    <Box sx={{ bgcolor: 'background.paper', width: '411px', border: '1px solid #c7c7c7' }}>
-      <AppBar 
-        position="static"
-        sx={{
-          boxShadow: 'none',
-        }}
-      >
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="inherit"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-          sx={{
-            color: '#93989a',
-            background: '#d6d9dc',
-            minHeight: '45px',
-          }}
-          TabIndicatorProps={{
-            sx: {backgroundColor: 'transparent'},
-          }}
-        >
-          <Tab 
-            label="Содержимое" {...a11yProps(0)}
-            disableFocusRipple
-            disableRipple
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><path fill={value === 0 ? '#516167' : '#93989a'} d="M9.566 9.22H.86V.516h8.705V9.22ZM2.803 7.291h4.834V2.457H2.803v4.834Zm8.691 1.929V.516H20.2V9.22h-8.705Zm1.942-1.929h4.834V2.457h-4.834v4.834Zm-3.87 12.562H.86v-8.704h8.705v8.704Zm-6.763-1.929h4.834V13.09H2.803v4.834Zm17.396 1.929h-8.705v-8.704H20.2v8.704Zm-6.763-1.929h4.834V13.09h-4.834v4.834Z"></path></svg>}
-            iconPosition="start"
-            sx={{
-              minWidth: 'max-content',
-              minHeight: '45px',
-              fontSize: '13px',
-              padding: '12px 5px',
-              background: value === 0 ? '#f9f9f9' : '#d6d9dc',
-              color: value === 0 ? '#516167' : '#93989a',
-              borderRight: '1px solid #c7c7c7',
-              borderBottom: value === 0 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              transition: 'borderBottom 0s',
-              '&:focus': {
-                outline: 'none',
-              },
-              '&:hover': {
-                border: 'none',
-                borderRight: '1px solid #c7c7c7',
-                borderBottom: value === 0 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              }
-            }} 
-          />
-          <Tab 
-            label="Строки" {...a11yProps(1)}
-            disableFocusRipple
-            disableRipple
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path fill={value === 1 ? '#516167' : '#93989a'} d="M.154 9.181V.516h19.338V9.18H.154ZM1.93 7.253h15.814V2.444H1.929v4.809ZM4.5 4.14h11.057v1.415H4.5V4.14ZM.193 19.88v-8.666H19.53v8.666H.193Zm1.774-1.968h15.827v-4.847H1.967v4.847ZM4.36 14.84h11.057v1.414H4.359v-1.414Z"></path></svg>}
-            iconPosition="start"
-            sx={{
-              minWidth: 'max-content',
-              minHeight: '45px',
-              fontSize: '13px',
-              background: value === 1 ? '#f9f9f9' : '#d6d9dc',
-              color: value === 1 ? '#516167' : '#93989a',
-              borderRight: '1px solid #c7c7c7',
-              borderBottom: value === 1 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              transition: 'borderBottom 0s',
-              '&:focus': {
-                outline: 'none',
-              },
-              '&:hover': {
-                border: 'none',
-                borderRight: '1px solid #c7c7c7',
-                borderBottom: value === 1 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              }
-            }} 
-          />
-          <Tab 
-            label="Настройки" {...a11yProps(2)} 
-            disableFocusRipple
-            disableRipple
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path fill={value === 2 ? '#516167' : '#93989a'} d="M14.662 9.117H4.994v-.964h9.668v.964Zm0 1.942H4.994v.964h9.668v-.964Zm0 2.802H4.994v.965h9.668v-.965ZM19.394.516v19.337H.249V.516h19.145Zm-1.839 5.798H2.088v11.61h15.377V6.314h.09Zm0-3.96H2.088V5.35h15.377V2.354h.09Z"></path></svg>}
-            iconPosition="start"
-            sx={{
-              minWidth: 'max-content',
-              minHeight: '45px',
-              fontSize: '13px',
-              background: value === 2 ? '#f9f9f9' : '#d6d9dc',
-              color: value === 2 ? '#516167' : '#93989a',
-              borderBottom: value === 2 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              transition: 'borderBottom 0s',
-              '&:focus': {
-                outline: 'none',
-              },
-              '&:hover': {
-                border: 'none',
-                borderBottom: value === 2 ? '1px solid #f9f9f9' : '1px solid #c7c7c7',
-              }
-            }}
-          />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis="x"
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-            {ViewChild}
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-            Строки
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-            Настройки
-        </TabPanel>
-      </SwipeableViews>
-    </Box>
-  );
-}
 const LetterBuilderLeftSide = () => {
+  const LetterCardElementArray = LetterCardList.map((item) => {
+    return (
+      <LetterCard 
+        key={item.id}
+        text={item.text} 
+        icon={item.icon}
+      />
+    )
+  })
+  
   return (
     <Box sx={{ width: '500px', height: '100%' }}>
       <FullWidthTabs 
-        ViewChild={
-          <LetterCard 
-            text="ЗАГОЛОВОК" 
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 30 26"><path d="m10.849 4.217-.051-.223a3.547 3.547 0 0 0-.288-.746c-.15-.307-.3-.58-.466-.851a5.165 5.165 0 0 0-.563-.707c-.197-.185-.35-.266-.477-.29a23.179 23.179 0 0 0-.703-.051 12.724 12.724 0 0 0-.793-.017H7.26v10.445c0 .184.03.313.09.405l.01.015.008.016c.047.096.14.182.321.275.019.01.075.03.194.058.112.027.257.057.438.092.375.073.618.118.773.118h.286V14H2.603v-1.216l.257-.027.287-.025c.146-.012.32-.027.507-.045.347-.034.558-.082.663-.121.165-.072.25-.145.315-.243.058-.089.091-.214.091-.458V1.315h-.249c-.19 0-.422 0-.688.016-.275.018-.55.035-.806.053-.102.01-.245.086-.46.289a5.287 5.287 0 0 0-1.024 1.563c-.138.316-.233.56-.278.745l-.053.219H0V0h12v4.217h-1.152ZM16 0h14v2H16zM16 6h14v2H16zM16 12h14v2H16zM0 18h30v2H0zM0 24h30v2H0z"></path></svg>}
-          />
-        }
+        TabList={TabList}
+        ViewChild={LetterCardElementArray}
       />
     </Box>
   )
