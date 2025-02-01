9dd006cfcda725ea13d0446d6b194fb82ca4a9f0
Petr Chukhnov <peka4u@vk.com>
2025-02-02 01:10:02 +0300

    Completed Video fix

--- letter-builder/atoms/LineBlocksContent/Video/Video.tsx
+++ letter-builder/atoms/LineBlocksContent/Video/Video.tsx
@@ -3,74 +3,71 @@ import { useDispatch, useSelector } from "react-redux";
 import { IconPngVideo } from "@components/atoms/Icons/LetterCardIcons";
 import { RootState } from "@/store/store";
 import { setVideoUrl, toggleSidebar } from "../../../reducers/videoSlice";
-import TextField from "@mui/material/TextField";
+import { TextField, FormGroup, FormControlLabel, Switch } from "@mui/material";
 
 const VideoComponent = () => {
   const dispatch = useDispatch();
   const { isSidebarOpen, videoUrl } = useSelector((state: RootState) => state.video);
-
+  const [advancedPadding, setAdvancedPadding] = useState(false);
   const [videoTitle, setVideoTitle] = useState("");
   const [padding, setPadding] = useState({
     top: 10,
     right: 10,
     bottom: 10,
     left: 10,
+    all: 10,
   });
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     dispatch(setVideoUrl(event.target.value));
-  };
 
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setVideoTitle(event.target.value);
-  };
-
-  const handlePaddingChange = (side: string, value: number) => {
-    setPadding((prevPadding) => ({
-      ...prevPadding,
-      [side]: value,
-    }));
-  };
+  const handlePaddingChange = (side: string, value: number) =>
+    setPadding((prev) => ({ ...prev, [side]: value }));
 
   const getYouTubeEmbedUrl = (url: string) => {
-    const videoIdMatch = url.match(
-      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([\w-]+)/,
-    );
-    return videoIdMatch ? `https://www.youtube.com/embed/${videoIdMatch[1]}` : "";
+    const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/))([\w-]+)/);
+    return match ? `https://www.youtube.com/embed/${match[1]}` : "";
   };
-
-  const iconStyle = { color: "#515659", scale: 1.3 };
 
   return (
     <div className="video-component">
-      {!videoUrl && !isSidebarOpen && (
-        <div onClick={() => dispatch(toggleSidebar(true))} className="video-icon">
-          <div className="text-[#515659]">
-            <div className="flex items-center justify-center">
-              <IconPngVideo {...iconStyle} />
-            </div>
-            <br />
-            Видео
+      {!isSidebarOpen && !videoUrl && (
+        <div onClick={() => dispatch(toggleSidebar(true))} className="video-icon text-[#515659]">
+          <div className="flex items-center justify-center">
+            <IconPngVideo style={{ color: "#515659", scale: 1.3 }} />
           </div>
+          <br />
+          Видео
         </div>
       )}
 
       {videoUrl && (
-        <iframe
-          width="100%"
-          height="100%"
-          src={getYouTubeEmbedUrl(videoUrl)}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          style={{
-            paddingTop: `${padding.top}px`,
-            paddingRight: `${padding.right}px`,
-            paddingBottom: `${padding.bottom}px`,
-            paddingLeft: `${padding.left}px`,
-          }}
-        ></iframe>
+        <>
+          <iframe
+            width="100%"
+            height="100%"
+            src={getYouTubeEmbedUrl(videoUrl)}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={
+              advancedPadding
+                ? {
+                    paddingTop: padding.top,
+                    paddingRight: padding.right,
+                    paddingBottom: padding.bottom,
+                    paddingLeft: padding.left,
+                  }
+                : { padding: padding.all }
+            }
+          ></iframe>
+          {videoTitle && (
+            <div style={{ textAlign: "center", marginTop: "8px", fontWeight: "bold" }}>
+              {videoTitle}
+            </div>
+          )}
+        </>
       )}
 
       {isSidebarOpen && (
@@ -84,104 +81,136 @@ const VideoComponent = () => {
             value={videoUrl}
             onChange={handleInputChange}
             fullWidth
-            style={{
-              marginBottom: "8px",
-            }}
+            style={{ marginBottom: "8px" }}
           />
-
-          <div
-            style={{
-              fontSize: "10px",
-              backgroundColor: "transparent",
-              textAlign: "left",
-            }}
-          >
-            <p>
-              Добавьте YouTube URL для автоматической генерации картинки. Изображение будет
-              ссылаться на предоставленный URL.
-            </p>
-          </div>
-
           <TextField
             label="Название видео"
             variant="filled"
             value={videoTitle}
-            onChange={handleTitleChange}
+            onChange={(e) => setVideoTitle(e.target.value)}
             fullWidth
-            style={{
-              marginBottom: "8px",
-            }}
+            style={{ marginBottom: "8px" }}
           />
 
           <div className="padding-editor" style={{ marginTop: "20px", flexGrow: 1 }}>
             <h4 style={{ fontSize: "12px", textAlign: "left" }}>Отступы видео</h4>
-            <div
-              className="padding-controls"
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <div>
-                <label>Сверху:</label>
-                <input
-                  type="number"
-                  value={padding.top}
-                  onChange={(e) => handlePaddingChange("top", +e.target.value)}
-                  style={{
-                    width: "50px",
-                    marginBottom: "8px",
-                    backgroundColor: "transparent",
-                    border: "1px solid black",
-                  }}
-                />
-              </div>
-              <div>
-                <label>Справа:</label>
-                <input
-                  type="number"
-                  value={padding.right}
-                  onChange={(e) => handlePaddingChange("right", +e.target.value)}
-                  style={{
-                    width: "50px",
-                    marginBottom: "8px",
-                    backgroundColor: "transparent",
-                    border: "1px solid black",
-                  }}
-                />
-              </div>
-            </div>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={advancedPadding}
+                    onChange={(e) => setAdvancedPadding(e.target.checked)}
+                  />
+                }
+                label="Больше свойств"
+              />
+            </FormGroup>
+            {advancedPadding ? (
+              <>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div>
+                    {/* <label>Сверху:</label>
+                     */}
+                    <TextField
+                      id="filled-number"
+                      label="Сверху"
+                      type="number"
+                      variant="filled"
+                      slotProps={{
+                        inputLabel: {
+                          shrink: true,
+                        },
+                      }}
+                      style={{
+                        margin: "4px",
+                      }}
+                      value={padding.top}
+                      onChange={(e) => handlePaddingChange("top", +e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    {/* <label>Справа:</label> */}
+                    <TextField
+                      id="filled-number"
+                      label="Справа"
+                      type="number"
+                      variant="filled"
+                      slotProps={{
+                        inputLabel: {
+                          shrink: true,
+                        },
+                      }}
+                      value={padding.right}
+                      onChange={(e) => handlePaddingChange("right", +e.target.value)}
+                      style={{
+                        margin: "4px",
+                      }}
+                    />
+                  </div>
+                </div>
 
-            <div
-              className="padding-controls"
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div>
+                    {/* <label>Снизу:</label> */}
+                    <TextField
+                      id="filled-number"
+                      label="Снизу"
+                      type="number"
+                      variant="filled"
+                      slotProps={{
+                        inputLabel: {
+                          shrink: true,
+                        },
+                      }}
+                      value={padding.bottom}
+                      onChange={(e) => handlePaddingChange("bottom", +e.target.value)}
+                      style={{
+                        margin: "4px",
+                      }}
+                    />
+                  </div>
+                  <div>
+                    {/* <label>Слева:</label> */}
+                    <TextField
+                      id="filled-number"
+                      label="Слева"
+                      type="number"
+                      variant="filled"
+                      slotProps={{
+                        inputLabel: {
+                          shrink: true,
+                        },
+                      }}
+                      value={padding.left}
+                      onChange={(e) => handlePaddingChange("left", +e.target.value)}
+                      style={{
+                        margin: "4px",
+                      }}
+                    />
+                  </div>
+                </div>
+              </>
+            ) : (
               <div>
-                <label>Снизу:</label>
-                <input
+                {/* <label>Все стороны:</label> */}
+                <TextField
+                  id="filled-number"
+                  label="Все стороны"
                   type="number"
-                  value={padding.bottom}
-                  onChange={(e) => handlePaddingChange("bottom", +e.target.value)}
+                  variant="filled"
+                  slotProps={{
+                    inputLabel: {
+                      shrink: true,
+                    },
+                  }}
+                  value={padding.all}
+                  onChange={(e) => handlePaddingChange("all", +e.target.value)}
                   style={{
-                    width: "50px",
-                    marginBottom: "8px",
-                    backgroundColor: "transparent",
-                    border: "1px solid black",
+                    margin: "4px",
                   }}
                 />
               </div>
-              <div>
-                <label>Слева:</label>
-                <input
-                  type="number"
-                  value={padding.left}
-                  onChange={(e) => handlePaddingChange("left", +e.target.value)}
-                  style={{
-                    width: "50px",
-                    marginBottom: "8px",
-                    backgroundColor: "transparent",
-                    border: "1px solid black",
-                  }}
-                />
-              </div>
-            </div>
+            )}
           </div>
 
           <button
@@ -191,11 +220,10 @@ const VideoComponent = () => {
               width: "100%",
               padding: "5px",
               fontSize: "12px",
-              color: "#000",
               border: "1px solid #ccc",
               backgroundColor: "transparent",
-              cursor: "pointer",
               marginTop: "8px",
+              cursor: "pointer",
             }}
           >
             Скрыть редактор
