d7e88eefd7aff1cd2afeb2ec3f2b7ae6d582a043
thatwasgen1us <skelletrise@gmail.com>
2025-04-10 20:44:37 +0700

    изменил конфиг eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true,
+    jest: true 
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended' 
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'jest' // Добавьте эту строку
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
   },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.tsx', '**/*.spec.js', '**/*.spec.tsx'],
+      env: {
+        jest: true
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ]
 }
