bb579361816223f9ca70601e4bec6864e9dffc27
Artem <skeleton.ubica@gmail.com>
2025-10-03 03:42:55 +0300

    закоментировал бэк

--- backendSrc/apps/posts/urls.ts
+++ backendSrc/apps/posts/urls.ts
@@ -1,6 +1,6 @@
-import { RouterType } from "../../router";
-import { GetPostsApi } from "./api/getPosts";
+// import { RouterType } from "../../router";
+// import { GetPostsApi } from "./api/getPosts";
 
-export default function registerPostsRoutes(router: RouterType) {
-  router.get("/api/posts/", GetPostsApi);
-}
+// export default function registerPostsRoutes(router: RouterType) {
+//   router.get("/api/posts/", GetPostsApi);
+// }
--- backendSrc/router.ts
+++ backendSrc/router.ts
@@ -1,13 +1,13 @@
-import { Router } from 'itty-router'
-import { fromIttyRouter } from 'chanfana'
+// import { Router } from 'itty-router'
+// import { fromIttyRouter } from 'chanfana'
 
-const router_ = Router()
-export const router = fromIttyRouter(router_)
-router.registry.registerComponent('securitySchemes', 'BearerAuth', {
-    type: 'http',
-    scheme: 'bearer',
-    bearerFormat: 'JWT'
-})
+// const router_ = Router()
+// export const router = fromIttyRouter(router_)
+// router.registry.registerComponent('securitySchemes', 'BearerAuth', {
+//     type: 'http',
+//     scheme: 'bearer',
+//     bearerFormat: 'JWT'
+// })
 
-export type RouterType = typeof router
+// export type RouterType = typeof router
 
