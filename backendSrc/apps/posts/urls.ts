8f81ba6e2767ac16f819493a9a123cc464c5b666
Artem <skeleton.ubica@gmail.com>
2025-10-08 00:50:03 +0300

    добавил tests

--- backendSrc/apps/posts/urls.ts
+++ backendSrc/apps/posts/urls.ts
@@ -3,4 +3,10 @@ import { GetPostsApi } from "./api/getPosts";
 
 export default function registerPostsRoutes(router: RouterType) {
   router.get("/api/posts/", GetPostsApi);
+  router.get("/api/posts/test", () => {
+    return Response.json([
+      { id: 1, title: "Test Post 1", content: "Test content 1" },
+      { id: 2, title: "Test Post 2", content: "Test content 2" },
+    ]);
+  });
 }
